a7d504296d24e6bd6cfcf96089128c49efdedb87
delaemon <takeshi.music.5@gmail.com>
2015-09-12 13:49:45 +0900

    Update output with color

--- src/bingo.go
+++ src/bingo.go
@@ -4,7 +4,25 @@ import (
 	"sort"
 	"math/rand"
 	"time"
+	"strconv"
 )
+
+const (
+	Red    = "0;31m"
+	Green  = "0;32m"
+	Yellow = "0;33m"
+	Blue   = "0;34m"
+	PurPle = "0;35m"
+	Cyan   = "0;36m"
+)
+
+var (
+	debug bool
+)
+
+func printColor(out, color string) {
+	fmt.Printf("\x1b[%s%s\x1b[0m", color, out)
+}
 
 func getCard() map[int][]int {
 	card := make(map[int][]int)
@@ -66,6 +84,86 @@ func grid(m map[int][]int) {
 	}
 }
 
+func gridHit(m map[int][]int, x ,y int) {
+	keys := keys(m)
+	sort.Sort(sort.IntSlice(keys))
+
+	length := len(m[0]) * 3 + 1
+	line(length)
+	for row := 0; row < len(m[0]); row++ {
+		fmt.Print("|")
+		for _, col := range keys {
+			if x == col && y == row {
+				printColor(fmt.Sprintf("%2d", m[col][row]), Green)
+			} else {
+				fmt.Printf("%2d", m[col][row])
+			}
+			fmt.Print("|")
+		}
+		fmt.Println("")
+		line(length)
+	}
+}
+
+func gridReach(m map[int][]int, hits [][2]int) {
+	keys := keys(m)
+	sort.Sort(sort.IntSlice(keys))
+
+	length := len(m[0]) * 3 + 1
+	line(length)
+	h := 0
+	for row := 0; row < len(m[0]); row++ {
+		fmt.Print("|")
+		for _, col := range keys {
+			if m[col][row] == 0 {
+				if len(hits[h]) > 0 && col == hits[h][0] && row == hits[h][1] {
+					if h < (len(hits) - 1) {
+						h++
+					}
+					printColor(fmt.Sprintf("%2d", m[col][row]), Yellow)
+				} else {
+					fmt.Printf("%2d", m[col][row])
+				}
+			} else {
+				fmt.Printf("%2d", m[col][row])
+			}
+			fmt.Print("|")
+		}
+		fmt.Println("")
+		line(length)
+	}
+
+}
+
+func gridGoal(m map[int][]int, hits [][2]int) {
+	keys := keys(m)
+	sort.Sort(sort.IntSlice(keys))
+
+	length := len(m[0]) * 3 + 1
+	line(length)
+	h := 0
+	for row := 0; row < len(m[0]); row++ {
+		fmt.Print("|")
+		for _, col := range keys {
+			if m[col][row] == 0 {
+				if len(hits[h]) > 0 && col == hits[h][0] && row == hits[h][1] {
+					if h < (len(hits) - 1) {
+						h++
+					}
+					printColor(fmt.Sprintf("%2d", m[col][row]), Red)
+				} else {
+					printColor(fmt.Sprintf("%2d", m[col][row]), Cyan)
+				}
+			} else {
+				fmt.Printf("%2d", m[col][row])
+			}
+			fmt.Print("|")
+		}
+		fmt.Println("")
+		line(length)
+	}
+}
+
 func shuffle(list []int) {
 	rand.Seed(time.Now().UnixNano())
 	for i := range list {
@@ -90,88 +188,84 @@ func hit(target int, card map[int][]int) (bool, int, int) {
 	return false, 0, 0
 }
 
-func goal(card map[int][]int, col int, row int) bool {
-
-	if goalCol(card, col) {
-		return true
-	}
-
-	if goalRow(card, row) {
-		return true
-	}
-
-	if goalRightOblique(card) {
-		return true
-	}
-
-	if goalLeftOblique(card) {
-		return true
-	}
-
-	return false
-}
-
-func goalLeftOblique(card map[int][]int) bool {
-	goal := true
+func goalLeftOblique(card map[int][]int) [][2]int {
+	line := make([][2]int, 0, 5)
 	row := 0
 	col := 0
 	for i := 0; i < 5; i++ {
 		row, col = i, i
-		fmt.Printf("[\\] col: %d, row: %d, val: %d\n", col, row, card[col][row])
-		if card[col][row] != 0 {
-			goal = false
-			break
+		if debug {
+			fmt.Printf("[\\] col: %d, row: %d, val: %d\n", col, row, card[col][row])
+		}
+		if card[col][row] == 0 {
+			line = append(line, [2]int{col, row})
 		}
 	}
-	return goal
+	return line
 }
 
-func goalRightOblique(card map[int][]int) bool {
-	goal := true
+func goalRightOblique(card map[int][]int) [][2]int {
+	line := make([][2]int, 0, 5)
 	row := 0
 	col := 0
 	for i := 4; i >= 0; i-- {
 		row, col = 4 - i, i
-		fmt.Printf("[/] col: %d, row: %d, val: %d\n", col, row, card[col][row])
-		if card[col][row] != 0 {
-			goal = false
-			break
+		if debug {
+			fmt.Printf("[/] col: %d, row: %d, val: %d\n", col, row, card[col][row])
+		}
+		if card[col][row] == 0 {
+			line = append(line, [2]int{col, row})
 		}
 	}
-	return goal
+	return line
 }
 
-func goalCol(card map[int][]int, col int) bool {
-	goal := true
+func goalCol(card map[int][]int, col int) [][2]int {
+	line := make([][2]int, 0, 5)
 	for row := 0; row < 5; row++ {
-		fmt.Printf("[|] col: %d, row: %d, val: %d\n", col, row, card[col][row])
-		if card[col][row] != 0 {
-			goal = false
-			break
+		if debug {
+			fmt.Printf("[|] col: %d, row: %d, val: %d\n", col, row, card[col][row])
+		}
+		if card[col][row] == 0 {
+			line = append(line, [2]int{col, row})
 		}
 	}
-	return goal
+	return line
 }
 
-func goalRow(card map[int][]int, row int) bool {
-	goal := true
+func goalRow(card map[int][]int, row int) [][2]int {
+	line := make([][2]int, 0, 5)
 	for col := 0; col < 5; col++ {
-		fmt.Printf("[-] col: %d, row: %d, val: %d\n", col, row, card[col][row])
-		if card[col][row] != 0 {
-			goal = false
-			break
+		if debug {
+			fmt.Printf("[-] col: %d, row: %d, val: %d\n", col, row, card[col][row])
+		}
+		if card[col][row] == 0 {
+			line = append(line, [2]int{col, row})
 		}
 	}
-	return goal
+	return line
+}
+
+func result(player int, card map[int][]int, line[][2]int) bool {
+	switch len(line) {
+	case 5:
+		printColor("player: " + strconv.Itoa(player + 1) + ", Goal!!!\n", Red)
+		gridGoal(card, line)
+		return true
+	case 4:
+		printColor("player: " + strconv.Itoa(player + 1) + ", Reach!!\n", Yellow)
+		gridReach(card, line)
+	}
+	return false
 }
 
 func main() {
 	cards := make([]map[int][]int, 0, 2)
 	p1_card := getCard()
 	p2_card := getCard()
-	fmt.Println("player1")
+	printColor("player: 1 Start\n", Blue)
 	grid(p1_card)
-	fmt.Println("player2")
+	printColor("player: 2 Start\n", Blue)
 	grid(p2_card)
 	cards = append(cards, p1_card)
 	cards = append(cards, p2_card)
@@ -187,18 +281,24 @@ func main() {
 			target := drawing(numbers, i)
 			hit, col, row := hit(target, card)
 			if hit {
-				fmt.Println("\nhit!!")
-				grid(card)
-				fmt.Printf("player: %d, target: %d, hit: %t, row:%d, col:%d\n",
-					player+1, target, hit, col, row)
+				printColor("player: " + strconv.Itoa(player + 1) + ", Hit!\n", Green)
+				gridHit(card, col, row)
+				if debug {
+					fmt.Printf("player: %d, target: %d, hit: %t, row:%d, col:%d\n",
+						player + 1, target, hit, col, row)
+				}
 			}
 			if hit {
-				goal := goal(card, col, row)
-				if goal {
-					fmt.Printf("player: %d goal!!!\n", player+1)
-					return
-				}
+				hits := goalCol(card, col)
+				if result(player, card, hits) { return }
+				hits = goalRow(card, row)
+				if result(player, card, hits) { return }
+				hits = goalRightOblique(card)
+				if result(player, card, hits) { return }
+				hits = goalLeftOblique(card)
+				if result(player, card, hits) { return }
 			}
 		}
 	}
 }
+
